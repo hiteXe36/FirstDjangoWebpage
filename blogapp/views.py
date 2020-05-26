cc550e71cf0c0df2610274af0132b69913c2e2a3
hiteXe36 <p.mathia@outlook.de>
2020-05-26 10:03:53 +0300

    added some database features

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-# dummy data
-posts = [
-    {
-        'author': 'Paul Mathia',
-        'title': 'Blog Post1',
-        'content': 'first post content',
-        'date_posted': 'May 25, 2020'
-    },
-    {
-        'author': 'Gigi',
-        'title': 'Blog Post2',
-        'content': 'second post content',
-        'date_posted': 'May 29, 2020'
-    }
-]
-
 
 def home(request):
     context = {
